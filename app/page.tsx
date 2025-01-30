eaf8b6be3ae534dd87ae7c24e58f0005027d56a6
Nacer MEDIOUNI <nacer.mediouni@gmail.com>
2025-01-30 11:57:27 +0100

    Add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,20 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Chatbot from "@/components/chatbot";
 export default function Home() {
-  return <main className="App">Hello there!</main>;
+  return (
+    <main className="App">
+      <div className="w-full flex flex-col items-center justify-center min-h-screen py-2">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-6xl font-bold mt-10">
+            Interacting with websites via AI Agent
+          </h1>
+          <Button className="mt-10 text-white border border-white hover:text-white  py-2 px-4 rounded-xl transition duration-300 ease-in-out">
+            Get Started
+          </Button>
+        </div>
+      </div>
+    </main>
+  );
 }
